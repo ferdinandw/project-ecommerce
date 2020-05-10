63837fab95b1e68ea63a7e8fa06eeab31dccd16c
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-10 14:15:22 +0700

    alvi FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/Navbar1'
+import Navbar0 from './components/navbar/Navbar0'
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
-function App() {
+
+
+class App extends Component{
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar0/>
+      <Navbar/>
+      <Carousel/>
+      <Footer/>
+    </Router>
   );
+}
 }
 
 export default App;
