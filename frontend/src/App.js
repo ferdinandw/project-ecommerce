ae4392d8cc78ac9b0488ee8132214c3709455525
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-11 17:48:41 +0700

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,32 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar/Navbar1";
+import Navbar0 from "./components/navbar/Navbar0";
 
-import {BrowserRouter as Router} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar/Navbar1'
-import Navbar0 from './components/navbar/Navbar0'
+import Carousel from "./components/Carousel";
+import Description from "./components/Description";
+import Footer from "./components/Footer";
 
-import Carousel from './components/Carousel';
-import Description from './components/Description'
-import Footer from './components/Footer';
-
-
-
-
-
-
-class App extends Component{
-  render(){
-  return (
-    <Router>
-      <Navbar0/>
-      <Navbar/>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar0 />
+        <Navbar />
         <Switch>
           <Route path="/new">
-            <Description/>
+            <Description />
           </Route>
           <Route path="/">
-            <Carousel/>
+            <Carousel />
           </Route>
         </Switch>
-      <Footer/>
-    </Router>
-
-  );
-}
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
