c4a4805dd6e523b4a715aca16bc184d97d184e1f
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-13 15:02:25 +0700

    mergegit

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-<<<<<<< HEAD:frontend/src/components/Login.js
   return (
     <div>
       <h1>Login</h1>
@@ -33,39 +32,6 @@ const Login = () => {
               <Link to="/register"> Sign Up For Free</Link>
             </h6>
           </div>
-=======
-    return(
-        <div>
-            <h1>Login</h1>
-            <div className="container-fluid">
-                <div className="row test">
-                    <div className="col-md-6">
-                        <img src={require("../img/bri.png")} alt="Gambar 1" />
-                    </div>
-                    <div className="col-md-6">
-                        <form className="mb-4">
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" class="form-control" id="email"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password"/>
-                                <small class="form-text">
-                                    forgot password ?
-                                    <Link to="/recovery"> Click Here</Link>
-                                </small>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
-                        <h6>
-                            Don't Have an Account ? 
-                            <Link to="/register"> Sign Up For Free</Link>
-                        </h6>
-                    </div>
-                </div>
-            </div>
->>>>>>> e2812ae18d34aeddf32668f6a35c519152014f5f:frontend/src/components/login/Login.js
         </div>
       </div>
     </div>
