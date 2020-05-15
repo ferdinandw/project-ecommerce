038d5bfa0966ff69fb378e5fa24f715567b0fde8
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-15 10:44:38 +0700

    test card

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -67,7 +67,94 @@ class Home extends Component {
     let itemList = this.props.items.map((item) => {
       return (
         <div className="row">
-          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-3 sm-12">
+          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-4 sm-12">
+            <div className="card" style={{ width: "18rem" }} key={item.id}>
+              {" "}
+              <div className="card-image">
+                <img src={item.img} alt={item.title} className="card-img-top" />
+                <span className="card-title" style={{ textAlign: "center" }}>
+                  {item.title}
+                </span>
+              </div>
+              <div className="card-content">
+                <p>{item.desc}</p>
+                <p>
+                  <b>Price: {item.price}$</b>
+                </p>
+              </div>
+              <br />
+              <Detail />
+              <button
+                to="/"
+                className="btn btn-outline-dark"
+                onClick={() => {
+                  this.handleClick(item.id);
+                }}
+              >
+                <Link to="/cart">Add Item</Link>
+              </button>
+            </div>
+          </div>
+
+          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-4 sm-12">
+            <div className="card" style={{ width: "18rem" }} key={item.id}>
+              {" "}
+              <div className="card-image">
+                <img src={item.img} alt={item.title} className="card-img-top" />
+                <span className="card-title" style={{ textAlign: "center" }}>
+                  {item.title}
+                </span>
+              </div>
+              <div className="card-content">
+                <p>{item.desc}</p>
+                <p>
+                  <b>Price: {item.price}$</b>
+                </p>
+              </div>
+              <br />
+              <Detail />
+              <button
+                to="/"
+                className="btn btn-outline-dark"
+                onClick={() => {
+                  this.handleClick(item.id);
+                }}
+              >
+                <Link to="/cart">Add Item</Link>
+              </button>
+            </div>
+          </div>
+
+          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-4 sm-12">
+            <div className="card" style={{ width: "18rem" }} key={item.id}>
+              {" "}
+              <div className="card-image">
+                <img src={item.img} alt={item.title} className="card-img-top" />
+                <span className="card-title" style={{ textAlign: "center" }}>
+                  {item.title}
+                </span>
+              </div>
+              <div className="card-content">
+                <p>{item.desc}</p>
+                <p>
+                  <b>Price: {item.price}$</b>
+                </p>
+              </div>
+              <br />
+              <Detail />
+              <button
+                to="/"
+                className="btn btn-outline-dark"
+                onClick={() => {
+                  this.handleClick(item.id);
+                }}
+              >
+                <Link to="/cart">Add Item</Link>
+              </button>
+            </div>
+          </div>
+
+          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-4 sm-12">
             <div className="card" style={{ width: "18rem" }} key={item.id}>
               {" "}
               <div className="card-image">
