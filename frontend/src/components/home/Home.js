498323fae717803f0f29a17187523be67707d9e0
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-15 10:08:54 +0700

    card button update

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -66,15 +66,24 @@ class Home extends Component {
   render() {
     let itemList = this.props.items.map((item) => {
       return (
-        <div className="card" style={{ width: "18rem" }} key={item.id}>
-          <div className="col pt-4 mt-5 mb-2">
-            {" "}
-            <div className="card-image">
-              <img src={item.img} alt={item.title} className="card-img-top" />
-              <span className="card-title" style={{ textAlign: "center" }}>
-                {item.title}
-              </span>
+        <div className="row">
+          <div className="col my-2 mt-5 pt-2 pl-0 pr-0 md-3 sm-12">
+            <div className="card" style={{ width: "18rem" }} key={item.id}>
+              {" "}
+              <div className="card-image">
+                <img src={item.img} alt={item.title} className="card-img-top" />
+                <span className="card-title" style={{ textAlign: "center" }}>
+                  {item.title}
+                </span>
+              </div>
+              <div className="card-content">
+                <p>{item.desc}</p>
+                <p>
+                  <b>Price: {item.price}$</b>
+                </p>
+              </div>
               <br />
+              <Detail />
               <button
                 to="/"
                 className="btn btn-outline-dark"
@@ -84,13 +93,6 @@ class Home extends Component {
               >
                 <Link to="/cart">Add Item</Link>
               </button>
-              <Detail />
-            </div>
-            <div className="card-content">
-              <p>{item.desc}</p>
-              <p>
-                <b>Price: {item.price}$</b>
-              </p>
             </div>
           </div>
         </div>
