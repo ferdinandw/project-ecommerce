99d33fb9d868967131a84ba9ccc467644e506890
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-17 18:26:30 +0700

    item show

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,57 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+// import ReactDOM from 'react-dom';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar/Navbar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar/Navbar";
 // import Searches from './components/searchFilter/Searches'
-import Category from './components/category/Category'
-import Carousel from './components/carousel/Carousel';
-import Cart from './components/cart/Cart'
-import Footer from './components/footer/Footer';
-import Home from './components/home/Home'
-import ItemSell from './components/ItemSelling/ItemSell'
-import Login from './components/login/Login'
-import MiniCarousel from './components/carousel/MiniCarousel'
-import Register from './components/register/Register'
-import cartReducer from './components/reducer/CartReducer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import Category from "./components/category/Category";
+import Carousel from "./components/carousel/Carousel";
+import Cart from "./components/cart/Cart";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import ItemSell from "./components/ItemSelling/ItemSell";
+import Login from "./components/login/Login";
+import MiniCarousel from "./components/carousel/MiniCarousel";
+import Register from "./components/register/Register";
+import cartReducer from "./components/reducer/CartReducer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Item from "./components/reducer/item";
 const store = createStore(cartReducer);
-class App extends Component{
-  render(){
-  return (
-    <Router>
-      <Provider store={store}>
-      <Navbar/>
-      {/* <Searches /> */}
-        <Switch>
-          <Route path="/login">
-            <Login/>
-            <Category/>
-          </Route>
-          <Route path="/register">
-            <Register/>
-            <Category/>
-          </Route>
-          <Route path="/seller">
-            <ItemSell/>
-            <Category/>
-          </Route>
-          <Route path="/cart">
-            <Cart/>
-            <Category/>
-          </Route>
-          <Route path="/">
-            <Carousel/>
-            <MiniCarousel/>
-            <Home/>
-            <Category/>
-          </Route>
-        </Switch>
-        <Footer />
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Provider store={store}>
+          <Navbar />
+          {/* <Searches /> */}
+          <Switch>
+            <Route path="/login">
+              <Login />
+              <Category />
+            </Route>
+            <Route path="/register">
+              <Register />
+              <Category />
+            </Route>
+            <Route path="/seller">
+              <ItemSell />
+              <Category />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+              <Category />
+            </Route>
+            <Route path="/">
+              <Carousel />
+              <MiniCarousel />
+              <Home />
+              <Category />
+              <Item />
+            </Route>
+          </Switch>
+          <Footer />
         </Provider>
       </Router>
     );
---
+++ frontend/src/components/reducer/item.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Test = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.juliaveronica.com/item/show").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
+
+  const showData = data.map((data, id) => {
+    return (
+      <tr key={id}>
+        <td>{data.id}</td>
+        <td>
+          <img src={data.image} alt="" />
+        </td>
+        <td>{data.name}</td>
+        <td>{data.price}</td>
+        <td>{data.description}</td>
+        <td>{data.quantity}</td>
+        <td>{data.categoryId}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <table
+        className="table table-dark table-striped table-hover justify-content-center"
+        style={{ width: "70%" }}
+      >
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Description</th>
+            <th scope="col">quantity</th>
+            <th scope="col">categoryId</th>
+          </tr>
+        </thead>
+        <tbody>{showData}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Test;
