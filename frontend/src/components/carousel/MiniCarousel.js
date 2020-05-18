e531ea29fb2e30df73fe06f7f7792d20349ef0cb
ferdinandw <fwng123@gmail.com>
2020-05-18 23:30:17 +0700

    carousel

--- frontend/src/components/carousel/MiniCarousel.js
+++ frontend/src/components/carousel/MiniCarousel.js
@@ -7,15 +7,18 @@ import "./style.css";
 export default class Carousel extends Component {
   render() {
     const settings = {
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 5,
+      slidesToShow: 4,
       slidesToScroll: 5,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed : 2500
     };
     return (
       <div className="container">
-        <Slider {...settings} className="test">
+        <Slider {...settings}>
           <img src={require("../img/2.png")} alt="Gambar 1" />
           <img src={require("../img/1.png")} alt="Gambar 1" />
           <img src={require("../img/3.png")} alt="Gambar 1" />
